92875ddca20f0ca597cc63085c06c612ef3cde43
nasona <nasona20@mail.wlu.edu>
2019-09-16 12:03:31 -0400

    added csv reader to create song objects

--- graph_demo/main.py
+++ graph_demo/main.py
@@ -1,20 +1,27 @@
 from song_graph import Song
 from song_graph import Camelot
 from song_graph import SongGraph
+import csv
+
+SONGCSV = "songs-demo.csv"
+
+def createSongs():
+    songFile = open(SONGCSV)
+    songData = csv.reader(songFile, delimiter=',')
+    firstRow = next(songData)
+    songList = []
+    for line in songData:
+        #print(line[11])
+        if len(line[10]) == 2:
+            camelot = Camelot(int(line[10][0]), line[10][1])
+        if len(line[10]) == 3:
+            camelot = Camelot(int(line[10][:2]), line[10][2])
+        song = Song(line[0], line[1], line[2], line[3], int(line[4]), line[5], line[6], line[7], line[8], int(line[9]), camelot, int(line[11]))
+        songList.append(song)
+    return songList
 
 def main():
-    one = Song("1", "Taylor Swift", "New Romantics", "1989", 16, True, True, "Pop", "F Major", 122, Camelot(7,"B"), 2014)
-    two = Song("2", "Taylor Swift", "I Knew You Were Trouble", "Red", 4, False, True, "Country", "F# Major", 154, Camelot(2,"B"), 2012)
-    three = Song("3", "Taylor Swift", "Enchanted", "Speak Now", 9, False, False, "Country", "Ab Major", 164, Camelot(4,"B"), 2010)
-    four = Song("4", "Taylor Swift", "Style", "1989", 3, False, True, "Pop", "D Major", 95, Camelot(10,"B"), 2014)
-    five = Song("5", "Taylor Swift", "Haunted", "Speak Now", 12, False, False, "Country", "F Major", 162, Camelot(7,"B"), 2010)
-    six = Song("6", "Taylor Swift", "I Almost Do", "Red", 7, False, False, "Country", "E Major", 146, Camelot(12,"B"), 2012)
-    seven = Song("7", "Taylor Swift", "Wildest Dreams", "1989", 9, False, True, "Pop", "Ab Major", 140, Camelot(4,"B"), 2014)
-    eight = Song("8", "Taylor Swift", "Delicate", "reputation", 5, False, True, "Pop", "A Minor", 95, Camelot(8,"A"), 2017)
-    nine = Song("9", "Taylor Swift", "Miss Americana & the Heartbreak Prince", "Lover", 7, False, False, "Pop", "B Minor", 150, Camelot(10,"A"), 2019)
-    ten = Song("10", "Taylor Swift", "Forever & Always", "Fearless", 11, False, False, "Country", "Bb Major", 128, Camelot(6,"B"), 2008)
-    songList = [one, two, three, four, five, six, seven, eight, nine, ten]
-    graph = SongGraph(songList)
+    graph = SongGraph(createSongs())
     print(str(graph))
 
 if __name__ == "__main__":
