7f3ee358386d7b95ed4c0b83dc765022d9845cb5
nasona <nasona20@mail.wlu.edu>
2019-09-30 13:26:30 -0400

    implemented a star -- untested

--- graph_demo/graph_algs.py
+++ graph_demo/graph_algs.py
@@ -7,31 +7,53 @@ contains functions for the graph algorithms that will run to create
 the playlists
 """
 
-def aStar(graph, startVertex, endVertex):
+MINIMUM_PLAYLIST_LENGTH = 5
+def calculateEstimatedDistance(graph, layer):
+    return graph.maxPossibleEdgeWeight()//2 * (MINIMUM_PLAYLIST_LENGTH - layer)
+
+def aStar(graph):
     """implementing shortest paths algorithm"""
-    playGraph = graph
     open = []
-    closed = [startVertex]
-    #set start vertex distance to zero
+    closed = [graph.getStartVertex()]
+    tracker = {}
     for vertex in graph.vertices():
-        if vertex != startVertex:
+        if vertex == graph.getStartVertex():
+            tracker[vertex.getLabel()] = {"included": True, "distance": 0, "path":[]}
+        else:
             open.append(vertex)
-            if vertex in startVertex.neighboringVertices():
-                print("needs work")
-                #set row's distance cell to the edge's weight + dist to goal
-                #set the row's path to cell to source vertex
+            edge = vertex.getEdgeTo(graph.getStartVertex())
+            if edge != None:
+                tracker[vertex.getLabel()] = {"included": False, "distance": edge.getWeight(), "path":[vertex.getLabel()]}
             else:
-                print("needs work")
-                #set the row's distance cell to infinity
-                #set the row's path cell to undefined
+                tracker[vertex.getLabel()] = {"included": False, "distance": 0, "path":[]}
     while not open.isEmpty():
         #Find the vertex F that is not yet included and has the minimal distance
-        #Pop F from the open list
+        minLabel = None
+        for label in tracker:
+            if minLabel == None:
+                minLabel = tracker[label]
+            else:
+                if tracker[minLabel] > tracker[label]:
+                    #Pop F from the open list
+                    chosenVertex = open.pop(graph.getVertex(minLabel))
         #If F is the goal, stop processing
+        if chosenVertex = graph.getEndVertex():
+            break
         #For each other vertex T not included
+        for vertex in open:
+            edge = chosenVertex.getEdgeTo(vertex)
             #If there is an edge from F to T
+            if edge != None:
                 #Calculate a new distance: 	F's distance + edge's weight + estimated distance to goal
-            #If new distance < T's distance in the results list
-                #Set T's distance to new distance
-                #Set T's path to F
-	            #Push F onto the closed list
+                #tracker[vertex.getLabel()]["distance"]
+                newDistance = tracker[chosenVertex.getLabel()] + edge.getWeight() + calculateEstimatedDistance(graph, chosenVertex._layer)
+                #If new distance < T's distance in the results list
+                if newDistance < tracker[vertex.getLabel()]["distance"]:
+                    #Set T's distance to new distance
+                    tracker[vertex.getLabel()]["distance"] = newDistance
+                    #Set T's path to F
+                    tracker[vertex.getLabel()]["path"] = tracker[choosenVertex.getLabel()]["path"]
+                    tracker[vertex.getLabel()]["path"].append(choosenVertex.getLabel())
+	                #Push F onto the closed list
+                    closed.append(vertex)
+                    tracker[vertex.getLabel()]["included"] = True
